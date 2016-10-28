534912950cb0faf777f536d977556741dd11f3b4
kfrn <knfrances@gmail.com>
2016-10-28 14:22:42 +1300

    add merged HBS

--- db/data_Test.js
+++ db/data_Test.js
@@ -1,6 +1,6 @@
 { sentences:
   [
-    {id: 1, word: 'yo' , sentence: "What's up yo!"},
+    {id: 1, word: 'yo' , definiton:"it means hello", sentence: "What's up yo!"},
     {id: 1, word: 'bro' , sentence: "Hey bro"}
   ]
 ]}
